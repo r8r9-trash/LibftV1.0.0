61a867f902bd8ab526037a22b2af8830d3a6d2bf
r8r9-trash <117934402+r8r9-trash@users.noreply.github.com>
2022-11-11 18:08:16 +0100

    patching functions

---
+++ libft/ft_strdup.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbelleng <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 17:04:51 by tbelleng          #+#    #+#             */
+/*   Updated: 2022/11/11 14:42:45 by tbelleng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strcpy(char *dest, const char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(const char *s)
+{
+	char	*str;
+	int		len;
+
+	len = 0;
+	while (s[len] != 0)
+		len++;
+	str = malloc(len * sizeof(char));
+	if (!str)
+		return (NULL);
+	ft_strcpy(str, (const char *)s);
+	return (str);
+}
